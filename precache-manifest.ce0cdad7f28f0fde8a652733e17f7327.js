9719bc7c961cf149071a222d6a3fa046a9450d2f
Starscape000 <42933289+Starscape000@users.noreply.github.com>
2020-04-15 15:47:15 +0800

    Add files via upload

---
+++ precache-manifest.ce0cdad7f28f0fde8a652733e17f7327.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773a954a692ccb4995bdd68c49803eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1508bfbddbe65aa5e8cf",
+    "url": "/static/css/2.edbd325d.chunk.css"
+  },
+  {
+    "revision": "e3addf501b68520bb451",
+    "url": "/static/css/main.d9e6cd96.chunk.css"
+  },
+  {
+    "revision": "1508bfbddbe65aa5e8cf",
+    "url": "/static/js/2.1d0e5843.chunk.js"
+  },
+  {
+    "revision": "c615a5d5315d3409dda21fe77ea01561",
+    "url": "/static/js/2.1d0e5843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3addf501b68520bb451",
+    "url": "/static/js/main.f9d34166.chunk.js"
+  },
+  {
+    "revision": "7f1069d9461dd3989d08",
+    "url": "/static/js/runtime-main.73986c71.js"
+  },
+  {
+    "revision": "a212c5821213a702e60c8c87a2251484",
+    "url": "/static/media/404.a212c582.jpg"
+  },
+  {
+    "revision": "a62dcffda72179cc1ef76f815a0cc27d",
+    "url": "/static/media/500.a62dcffd.png"
+  },
+  {
+    "revision": "a270731f9c19e435e322a42690b026c5",
+    "url": "/static/media/login.a270731f.png"
+  }
+]);
